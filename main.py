bd14bf3b9ecdb92193b747d40d92777dd6302292
Krischel Moret <krischelmoret@gmail.com>
2026-09-08 19:05:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+import sys
+import pygame
+from engine import MotorJuego
 
+
+def main():
+
+    pygame.init()
+    pygame.mixer.init()
+
+    motor = MotorJuego()
+    motor.ejecutar()
+
+    pygame.mixer.quit()
+    pygame.quit()
+    sys.exit()
+
+
+if __name__ == "__main__":
+    main()
+
+    
